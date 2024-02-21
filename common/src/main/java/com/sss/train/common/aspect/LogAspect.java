6ce17391764b9baee4573c9dc6c0b0365875eadb
Saddss <108515797+Saddss@users.noreply.github.com>
2024-02-21 21:53:17 +0800

    增加公共模块用来放置工具类拦截器aop等等

--- common/src/main/java/com/sss/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/sss/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.sss.train.member.aspect;
+package com.sss.train.common.aspect;
 
 import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -25,7 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class LogAspect {
     public LogAspect() {
-        System.out.println("Common LogAspect");
+        System.out.println("111 LogAspect");
     }
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
