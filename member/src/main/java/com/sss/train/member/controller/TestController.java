c9c93fe3ae20d48bf3b86ace7436cabc42d05b84
Saddss <108515797+Saddss@users.noreply.github.com>
2024-02-21 20:53:34 +0800

    新增member模块

--- member/src/main/java/com/sss/train/member/controller/TestController.java
+++ member/src/main/java/com/sss/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.sss.train;
+package com.sss.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello world111122222";
+        return "hello world!";
     }
 }
