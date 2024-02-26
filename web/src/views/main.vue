7939c34c9a46d862d8928ee2f507556029ad6e3f
Saddss <108515797+Saddss@users.noreply.github.com>
2024-02-26 13:24:30 +0800

    为axios增加登录相关拦截，请求时带上token，返回时校验返回码是不是401，在vue.config.js中增加如下配置使红色全屏报错信息不出现：devServer: {client: {overlay: false}}

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -12,7 +12,7 @@
         <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-          Content
+          会员总数: {{count}}
         </a-layout-content>
       </a-layout>
     </a-layout>
@@ -22,13 +22,26 @@
 import { defineComponent, ref } from 'vue';
 import TheHeaderView from "@/components/the-header.vue";
 import TheSiderView from "@/components/the-sider.vue";
+import axios from "axios";
+import {notification} from "ant-design-vue";
 export default defineComponent({
   components: {
     TheSiderView,
     TheHeaderView,
   },
   setup() {
+    const count = ref(0);
+    axios.get("/member/member/count").then((response) => {
+      let data = response.data;
+      if (data.success) {
+        count.value = data.content;
+      } else {
+        notification.error({ description: data.message });
+      }
+    });
+
     return {
+      count
     };
   },
 });
