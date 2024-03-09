97f41f75d5f85f2ec16fb633bcae387c80551a36
Saddss <108515797+Saddss@users.noreply.github.com>
2024-03-09 16:04:57 +0800

    火车车站相关代码生成测试

---
+++ business/src/main/java/com/sss/train/business/req/TrainStationQueryReq.java
@@ -0,0 +1,12 @@
+package com.sss.train.business.req;
+
+import com.sss.train.common.req.PageReq;
+
+public class TrainStationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainStationQueryReq{" +
+                "} " + super.toString();
+    }
+}
