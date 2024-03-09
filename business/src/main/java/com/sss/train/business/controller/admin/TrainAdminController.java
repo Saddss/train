4581a204782f2d514669d9dc7807948602b88d0a
Saddss <108515797+Saddss@users.noreply.github.com>
2024-03-09 15:55:52 +0800

    火车相关代码生成测试

---
+++ business/src/main/java/com/sss/train/business/controller/admin/TrainAdminController.java
@@ -0,0 +1,39 @@
+package com.sss.train.business.controller.admin;
+
+import com.sss.train.common.context.LoginMemberContext;
+import com.sss.train.common.resp.CommonResp;
+import com.sss.train.common.resp.PageResp;
+import com.sss.train.business.req.TrainQueryReq;
+import com.sss.train.business.req.TrainSaveReq;
+import com.sss.train.business.resp.TrainQueryResp;
+import com.sss.train.business.service.TrainService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/train")
+public class TrainAdminController {
+
+    @Resource
+    private TrainService trainService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody TrainSaveReq req) {
+        trainService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<TrainQueryResp>> queryList(@Valid TrainQueryReq req) {
+        PageResp<TrainQueryResp> list = trainService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        trainService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
