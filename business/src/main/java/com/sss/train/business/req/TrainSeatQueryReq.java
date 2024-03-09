bf3f4087fb4b2b1887254a9f69ffdd8f0f78c4f1
Saddss <108515797+Saddss@users.noreply.github.com>
2024-03-09 16:19:27 +0800

    火车座位相关代码生成测试

---
+++ business/src/main/java/com/sss/train/business/req/TrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package com.sss.train.business.req;
+
+import com.sss.train.common.req.PageReq;
+
+public class TrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
--- generator/src/main/java/com/sss/train/generator/gen/EnumGenerator.java
+++ generator/src/main/java/com/sss/train/generator/gen/EnumGenerator.java
@@ -2,6 +2,7 @@ package com.sss.train.generator.gen;
 
 
 import cn.hutool.core.util.StrUtil;
+import com.sss.train.business.enums.SeatColEnum;
 import com.sss.train.business.enums.SeatTypeEnum;
 import com.sss.train.business.enums.TrainTypeEnum;
 import com.sss.train.member.enums.PassengerTypeEnum;
@@ -27,6 +28,7 @@ public class EnumGenerator {
             toJson(PassengerTypeEnum.class, bufferObject, bufferArray);
             toJson(TrainTypeEnum.class, bufferObject, bufferArray);
             toJson(SeatTypeEnum.class, bufferObject, bufferArray);
+            toJson(SeatColEnum.class, bufferObject, bufferArray);
             StringBuffer buffer = bufferObject.append("\r\n").append(bufferArray);
             writeJs(buffer);
         } catch (Exception e) {
