9b6f2a590508a3263be3080a442818a793a87a91
Saddss <108515797+Saddss@users.noreply.github.com>
2024-03-12 22:12:50 +0800

    集成openfeign,为各个服务添加应用名（spring.application.name)，可搭配注册中心使用

--- batch/src/main/java/com/sss/train/batch/config/BatchApplication.java
+++ batch/src/main/java/com/sss/train/batch/config/BatchApplication.java
@@ -1,14 +1,18 @@
 package com.sss.train.batch.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.sss")
+@MapperScan("com.sss.train.*.mapper")
+@EnableFeignClients("com.sss.train.batch.feign")
 public class BatchApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
--- batch/src/main/java/com/sss/train/batch/controller/TestController.java
+++ batch/src/main/java/com/sss/train/batch/controller/TestController.java
@@ -1,12 +1,24 @@
 package com.sss.train.batch.controller;
 
+import com.sss.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+    @Resource
+    BusinessFeign businessFeign;
+
     @GetMapping("/hello")
-    public String hello(){
-        return "hello world! Business!";
+    public String hello() {
+        String businessHello = businessFeign.hello();
+        LOG.info(businessHello);
+        return "Hello World! Batch!";
     }
 }
+
