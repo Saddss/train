14946f1b9397e79910549081ad278df2eb1f4360
Saddss <108515797+Saddss@users.noreply.github.com>
2024-03-12 13:24:54 +0800

    为每日座位页面增加车次查询条件

--- business/src/main/java/com/sss/train/business/req/DailyTrainSeatQueryReq.java
+++ business/src/main/java/com/sss/train/business/req/DailyTrainSeatQueryReq.java
@@ -1,12 +1,26 @@
 package com.sss.train.business.req;
 
 import com.sss.train.common.req.PageReq;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainSeatQueryReq extends PageReq {
+
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
         return "DailyTrainSeatQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
                 "} " + super.toString();
     }
 }
