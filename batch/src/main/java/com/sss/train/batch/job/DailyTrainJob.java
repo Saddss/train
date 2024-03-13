563f1bd00081bc0dd1803b38349464c8acfbacc7
Saddss <108515797+Saddss@users.noreply.github.com>
2024-03-13 17:42:41 +0800

    生成每日车站车厢座位功能实现

--- batch/src/main/java/com/sss/train/batch/job/DailyTrainJob.java
+++ batch/src/main/java/com/sss/train/batch/job/DailyTrainJob.java
@@ -3,6 +3,7 @@ package com.sss.train.batch.job;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.RandomUtil;
+import com.sss.train.batch.feign.BusinessFeign;
 import com.sss.train.common.resp.CommonResp;
 import jakarta.annotation.Resource;
 import org.quartz.DisallowConcurrentExecution;
@@ -20,8 +21,8 @@ public class DailyTrainJob implements Job {
 
     private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
 
-//    @Resource
-//    BusinessFeign businessFeign;
+    @Resource
+    BusinessFeign businessFeign;
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
@@ -31,7 +32,7 @@ public class DailyTrainJob implements Job {
         Date date = new Date();
         DateTime dateTime = DateUtil.offsetDay(date, 15);
         Date offsetDate = dateTime.toJdkDate();
-//        CommonResp<Object> commonResp = businessFeign.genDaily(offsetDate);
-//        LOG.info("生成15天后的车次数据结束，结果：{}", commonResp);
+        CommonResp<Object> commonResp = businessFeign.genDaily(offsetDate);
+        LOG.info("生成15天后的车次数据结束，结果：{}", commonResp);
     }
 }
