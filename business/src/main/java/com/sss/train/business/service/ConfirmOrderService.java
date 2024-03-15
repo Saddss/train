0c7665336eade694d7fcd5ab40edd35fd7f7b633
Saddss <108515797+Saddss@users.noreply.github.com>
2024-03-15 18:49:01 +0800

    保存确认订单表，状态初始，更新前端枚举

--- business/src/main/java/com/sss/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/sss/train/business/service/ConfirmOrderService.java
@@ -1,10 +1,16 @@
 package com.sss.train.business.service;
+import java.util.Date;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.json.JSONObject;
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.sss.train.business.enums.ConfirmOrderStatusEnum;
+import com.sss.train.business.req.ConfirmOrderDoReq;
+import com.sss.train.common.context.LoginMemberContext;
 import com.sss.train.common.resp.PageResp;
 import com.sss.train.common.util.SnowUtil;
 import com.sss.train.business.domain.ConfirmOrder;
@@ -66,5 +72,40 @@ public class ConfirmOrderService {
 
     public void delete(Long id) {
         confirmOrderMapper.deleteByPrimaryKey(id);
+    }
+
+    public void doConfirm(ConfirmOrderDoReq req) {
+        // 省略业务数据校验，如：车次是否存在，余票是否存在，车次是否在有效期内，tickets条数>0，同乘客同车次是否已买过
+
+        // 保存确认订单表，状态初始
+        DateTime now = new DateTime();
+        ConfirmOrder confirmOrder = new ConfirmOrder();
+        confirmOrder.setCreateTime(now);
+        confirmOrder.setUpdateTime(now);
+        confirmOrder.setMemberId(LoginMemberContext.getId());
+        confirmOrder.setDate(req.getDate());
+        confirmOrder.setTrainCode(req.getTrainCode());
+        confirmOrder.setStart(req.getStart());
+        confirmOrder.setEnd(req.getEnd());
+        confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
+        confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
+        confirmOrder.setTickets(JSON.toJSONString(req.getTickets()));
+        confirmOrderMapper.insert(confirmOrder);
+        // 查出余票记录，需要得到真实的库存
+
+        // 扣减余票数量，并判断余票是否足够
+
+        // 选座
+
+        // 一个车箱一个车箱的获取座位数据
+
+        // 挑选符合条件的座位，如果这个车箱不满足，则进入下个车箱（多个选座应该在同一个车厢）
+
+        // 选中座位后事务处理：
+
+        // 座位表修改售卖情况sell；
+        // 余票详情表修改余票；
+        // 为会员增加购票记录
+        // 更新确认订单为成功
     }
 }
